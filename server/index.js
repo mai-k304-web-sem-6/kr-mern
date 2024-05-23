d967097faf5ab7fd7f6450e7c07c24ec04aeb28c
Александр Пысларь <62560925+AlexPyslar03@users.noreply.github.com>
2024-05-23 17:29:36 +0300

    add docker

--- server/index.js
+++ server/index.js
@@ -61,7 +61,7 @@ app.patch(
   PostController.update,
 );
 
-app.listen(process.env.PORT || 4444, (err) => {
+app.listen(process.env.PORT || 5000, (err) => {
   if (err) {
     return console.log(err);
   }
