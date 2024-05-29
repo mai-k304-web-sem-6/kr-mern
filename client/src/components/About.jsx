ef48fb83ff5f3d2a26c2c153e1fd24936a8f0ef8
Александр Пысларь <62560925+AlexPyslar03@users.noreply.github.com>
2024-05-30 02:02:27 +0300

    я починил

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,27 +1,25 @@
 import React from 'react';
-import { Typography, Container, Paper, Box, Avatar } from '@mui/material';
-import PersonIcon from '@mui/icons-material/Person';
+import { Typography, Box } from '@mui/material';
 
 function About() {
     return (
-        <Container maxWidth="md" style={{ marginTop: '40px' }}>
-            <Paper style={{ padding: '20px' }}>
-                <Box display="flex" flexDirection="column" alignItems="center">
-                    <Avatar style={{ width: '100px', height: '100px', marginBottom: '20px' }}>
-                        <PersonIcon style={{ fontSize: '50px' }} />
-                    </Avatar>
-                    <Typography variant="h4" component="h1" gutterBottom>
-                        Обо мне
-                    </Typography>
-                    <Typography variant="body1" align="center" paragraph>
-                        Привет! Я страстный веб-разработчик, глубоко заинтересованный в создании динамичных и привлекательных веб-приложений. Имея опыт разработки как интерфейсов, так и серверной части, я оттачивал свои навыки в различных технологиях и фреймворках.                    </Typography>
-                    <Typography variant="body1" align="center" paragraph>
-                        Это приложение для ведения блога - один из моих последних проектов, разработанный для обеспечения простого и интуитивно понятного интерфейса пользователя при создании, просмотре, редактировании и удалении записей. Построен с помощью React и материал-интерфейс на сайте, и Node.js бэкэнд MongoDB для хранения данных, это приложение демонстрирует свою способность к интеграции современных веб-технологий в сплоченную и работоспособную систему.                    </Typography>
-                    <Typography variant="body1" align="center" paragraph>
-                        Моя цель - постоянно учиться и расти как разработчика, берясь за сложные проекты, которые расширяют границы возможного с помощью веб-технологий. Спасибо, что посетили мой блог, и я надеюсь, что вы найдете его содержание информативным и вдохновляющим.                    </Typography>
-                </Box>
-            </Paper>
-        </Container>
+        <Box p={3}>
+            <Typography variant="h4" gutterBottom>
+                Обо мне
+            </Typography>
+            <Typography variant="body1" paragraph>
+                Здравствуйте! Меня зовут Александр Пысларь я студент МАИ группы М3О-310Б-21 и я разработчик программного обеспечения с опытом работы в области веб-разработки.
+            </Typography>
+            <Typography variant="body1" paragraph>
+                Этот проект - моя попытка создать сервис для публикации и управления постами. Он создан с использованием технологий JavaScript (ReactJS для фронтенда и Node.js с фреймворком Express.js для бэкенда), а база данных MongoDB используется для хранения постов.
+            </Typography>
+            <Typography variant="body1" paragraph>
+                Сервис позволяет пользователям создавать, просматривать, обновлять и удалять посты, а также осуществлять поиск постов по заголовку и тексту. Мы также добавили возможность сортировки постов по алфавиту.
+            </Typography>
+            <Typography variant="body1" paragraph>
+                Мы стараемся делать этот сервис максимально удобным и функциональным для пользователей, и мы надеемся, что он будет полезен для вас!
+            </Typography>
+        </Box>
     );
 }
 
--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { Grid, Card, CardContent, Typography, Button, CardActions } from '@mui/material';
+import { Grid, Card, CardContent, Typography, Button, CardActions, TextField, Box, MenuItem, Select, FormControl, InputLabel } from '@mui/material';
 import { getPosts } from '../services/api';
 
 function PostList() {
     const [posts, setPosts] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [sortBy, setSortBy] = useState('title');
+    const [sortOrder, setSortOrder] = useState('asc');
 
     useEffect(() => {
         fetchPosts();
@@ -13,15 +16,61 @@ function PostList() {
     const fetchPosts = async () => {
         try {
             const response = await getPosts();
-            setPosts(response.data);
+            let filteredPosts = response.data;
+
+            if (searchTerm) {
+                filteredPosts = filteredPosts.filter(post =>
+                    post.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                    post.text.toLowerCase().includes(searchTerm.toLowerCase())
+                );
+            }
+
+            filteredPosts.sort((a, b) => {
+                return sortOrder === 'asc' ? a[sortBy].localeCompare(b[sortBy]) : b[sortBy].localeCompare(a[sortBy]);
+            });
+
+            setPosts(filteredPosts);
         } catch (error) {
             console.error("Error fetching posts:", error);
         }
     };
 
+    const handleSearch = () => {
+        fetchPosts();
+    };
+
+    const handleSortChange = (event) => {
+        const value = event.target.value;
+        const newSortBy = value.split('-')[0];
+        const newSortOrder = value.split('-')[1];
+
+        setSortBy(newSortBy);
+        setSortOrder(newSortOrder);
+        fetchPosts();
+    };
+
     return (
         <div>
-            <h1>Посты</h1>
+            <Box mb={2}>
+                <TextField
+                    label="Поиск"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    variant="outlined"
+                    fullWidth
+                    size="small"
+                />
+                <Button variant="contained" color="primary" onClick={handleSearch} style={{ marginLeft: '10px' }}>Искать</Button>
+            </Box>
+            <Box mb={2}>
+                <FormControl fullWidth variant="outlined" size="small">
+                    <InputLabel>Сортир</InputLabel>
+                    <Select value={`${sortBy}-${sortOrder}`} onChange={handleSortChange} label="Sort By">
+                        <MenuItem value="title-desc">Заголовок от А до Я</MenuItem>
+                        <MenuItem value="title-asc">Заголовок от Я до А</MenuItem>
+                    </Select>
+                </FormControl>
+            </Box>
             <Grid container spacing={4}>
                 {posts.map(post => (
                     <Grid item key={post._id} xs={12} sm={6} md={4}>
